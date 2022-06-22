6d75bdc49063d821d58b8f95ddd3c7f88da6e229
shinhyocheol <zzzskssk1@naver.com>
2022-06-22 14:00:56 +0900

    # 메시지 큐 리스너 활성화, 수신 메시지 확인

--- src/main/java/com/example/app/SqsMessageListener.java
+++ src/main/java/com/example/app/SqsMessageListener.java
@@ -1,25 +1,25 @@
-//package com.example.app;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
-//import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
-//import org.springframework.messaging.handler.annotation.Headers;
-//import org.springframework.messaging.handler.annotation.Payload;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Map;
-//
-//@Slf4j
-//@Component
-//public class SqsMessageListener {
-//
-//    @SqsListener(value = "example-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-//    public void listen(@Payload String message,
-//                       @Headers Map<String, Object> headers) {
-//
-//        log.info("message : " + message);
-//        log.info("{}", headers);
-//    }
-//
-//
-//}
+package com.example.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@Component
+public class SqsMessageListener {
+
+    @SqsListener(value = "example-queue", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void listen(@Payload String message,
+                       @Headers Map<String, Object> headers) {
+
+        log.info("message : " + message);
+        log.info("{}", headers);
+    }
+
+
+}
